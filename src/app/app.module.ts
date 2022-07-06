600370762c59d4b124af8fbc89b23e0b360ba3c1
KAXIII <cata13es@gmail.com>
2022-07-06 18:42:31 +0200

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HTTP } from '@awesome-cordova-plugins/http';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    
     BrowserModule, 
+    //HTTP,
+    HttpClientModule,
     IonicModule.forRoot(), 
     AppRoutingModule,
   ],
--- src/app/random/random.page.ts
+++ src/app/random/random.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-random',
@@ -7,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RandomPage implements OnInit {
 
-  constructor() { }
+  constructor(public http: HttpClientModule) { 
+    /*let url = "https://www.reddit.com/r/gifs/new/.json?limit=10";
+
+    this.http.get(url).map(res => res.json()).subscribe(data => {
+
+        console.log(data);
+
+    });*/
+  }
 
   ngOnInit() {
   }
